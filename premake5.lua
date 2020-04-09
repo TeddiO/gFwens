a040b8a902522c8f10f31d83a407760dc28f5c05
Teddi <teddi.orange@gmail.com>
2020-04-09 21:39:53 +0100

    Added support for 64 bit, when it eventually happens

--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,6 @@
+-- x64 is experimental and not guaranteed to work. Pending on gmod actually getting 64 bit support. 
+-- win32 suffix doesn't change currently as we don't know what the plan will be for this. Similar to how 
+-- linux / osx still get the .dll extension even though yknow, those are the wrong binary suffixes for those OS's
 
 platformAlias = {
     ["windows"] = "win32",
@@ -7,6 +10,7 @@ platformAlias = {
 
 workspace "gfwens"
     configurations {"Debug", "Release"}
+    platforms {"x32", "x64"}
 
 project "gmsv_gfwens"
     kind "SharedLib"
@@ -15,22 +19,43 @@ project "gmsv_gfwens"
     files { "src/*.cpp", "src/*.h" }
 
     includedirs { "../gmodheaders/include", "../steamworks" }
-    links { "../steamworks/sdk/redistributable_bin/steam_api.lib"}
-    targetname ("gmsv_gfwens_" .. platformAlias[os.target()])
+    targetname ("gmsv_fwens_" .. platformAlias[os.target()])
     targetprefix ""
     targetextension ".dll"
 
-    filter { "configurations:Debug" }
+    -- Debug | x32 / x64
+    filter { "configurations:Debug", "platforms:x32" }
         defines { "DEBUG", "_CRT_SECURE_NO_WARNINGS" }
+        links { "../steamworks/sdk/redistributable_bin/steam_api.lib"}
         symbols "On"
-        targetdir "bin/debug/"
-        objdir "bin/debug/"
+        targetdir "bin/x32/debug/"
+        objdir "bin/x32/debug/"
+        architecture "x32"
 
+    filter { "configurations:Debug","platforms:x64" }
+        defines { "DEBUG", "_CRT_SECURE_NO_WARNINGS" }
+        links { "../steamworks/sdk/redistributable_bin/win64/steam_api64.lib"}
+        symbols "On"
+        targetdir "bin/x64/debug/"
+        objdir "bin/x64/debug/"
+        architecture "x64"
 
-    filter { "configurations:Release" }
+    -- Release | x32 / x64
+    filter { "configurations:Release","platforms:x32" }
         defines { "NDEBUG", "_CRT_SECURE_NO_WARNINGS" }
+        links { "../steamworks/sdk/redistributable_bin/steam_api.lib"}
         optimize "On"
-        targetdir "bin/release/"
-        objdir "bin/release/"
+        targetdir "bin/x32/release/"
+        objdir "bin/x32/release/"
         symbols "off"
-        
+        architecture "x32"
+
+    filter { "configurations:Release", "platforms:x64" }
+        defines { "NDEBUG", "_CRT_SECURE_NO_WARNINGS" }
+        links { "../steamworks/sdk/redistributable_bin/win64/steam_api64.lib"}
+        optimize "On"
+        targetdir "bin/x64/release/"
+        objdir "bin/x64/release/"
+        symbols "off"
+        architecture "x64"
+ 
