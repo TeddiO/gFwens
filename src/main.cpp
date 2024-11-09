2a176f2efb6eda5199d44647c7ded09ff7fbfaf3
Teddi <me@teddi.dev>
2024-11-09 22:50:50 +0000

    Not sure if this is needed on linux anymore...

--- src/main.cpp
+++ src/main.cpp
@@ -74,11 +74,13 @@ GMOD_MODULE_OPEN()
 	Fwens* fwenVar = Fwens::GetInstance();
 	fwenVar->SetLuaInstance(LUA);
 
+#ifndef __linux__
 	// Exists for changelevel purposes. Will silently fail during first boot of the server.
 	if (!fwenVar->GetSteamContextActive())
 	{
 		fwenVar->InitSteamAPIConnection();
 	}
+#endif
 
 	return 0;
 }
